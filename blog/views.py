c220b639687446d8b5d6cd79423351129251d2e3
Rustam <nurgaliev_rustam@namisoft.ru>
2018-03-21 09:48:46 +0300

    Tamerlan

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    context_instance = RequestContext(request)
+    if request.method == "POST":
+
+        if request.method == "POST" or 'q' in request.POST:
+            q = request.POST['id']
+            return render(request, 'blog/post.html', {})
+        else :
+          return render(request, 'blog/error.html', {})
+
+    else:
+        return render(request, 'blog/get.html', {})
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
