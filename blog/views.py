5c46d9035073bed5cb141bdd5b4f3edc19d0d95f
Rustam <nurgaliev_rustam@namisoft.ru>
2018-03-21 13:47:31 +0300

    Add respons

--- blog/views.py
+++ blog/views.py
@@ -10,8 +10,15 @@ def post_list(request):
     context_instance = RequestContext(request)
     if request.method == "POST":
 
-        if request.method == "POST" or 'q' in request.POST:
-            q = request.POST['id']
+        if 'id_device' in request.POST and 'token' in request.POST and \
+                'window_id' in request.POST and 'view_id' in request.POST and 'rate' in request.POST :
+            id_device = request.POST['id_device']
+            token = request.POST['token']
+            window_id = request.POST['window_id']
+            view_id = request.POST['view_id']
+            rate = request.POST['rate']
+            print(id_device)
+
             return render(request, 'blog/post.html', {})
         else :
           return render(request, 'blog/error.html', {})
