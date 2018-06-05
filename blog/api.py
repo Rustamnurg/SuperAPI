0f0bb01dbfb911e63d18fc6ecd785fdbdef72783
Rustam <nurgaliev_rustam@namisoft.ru>
2018-06-06 02:07:05 +0300

    Fix

--- blog/api.py
+++ blog/api.py
@@ -48,7 +48,7 @@ def report(request):
 
                 img_data = heatmap_base64
                 with open("blog/images/{0}.png".format(image_name), "wb") as fh:
-                     # fh.write(img_data.decode('base64'))
+                     fh.write(img_data.decode('base64'))
 
                 newReport.save()
                 print(views_blocks)
